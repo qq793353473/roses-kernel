5eb793b5431ac0c8610152bb0c3db456cc486222
fengshuonan <sn93@qq.com>
2018-07-24 20:32:34 +0800

    修改mysql连接

--- kernel-generator/src/main/java/com/stylefeng/roses/kernel/generator/GenerateParams.java
+++ kernel-generator/src/main/java/com/stylefeng/roses/kernel/generator/GenerateParams.java
@@ -17,7 +17,7 @@ public class GenerateParams {
 
     private String jdbcDriver = "com.mysql.jdbc.Driver";
 
-    private String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/guns?autoReconnect=true&useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=CONVERT_TO_NULL&useSSL=false&serverTimezone=UTC";
+    private String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/guns?autoReconnect=true&useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=false&serverTimezone=UTC";
 
     private String jdbcUserName = "root";
 
