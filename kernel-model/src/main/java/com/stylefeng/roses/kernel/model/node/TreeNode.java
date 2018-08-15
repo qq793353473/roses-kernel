db1f1a9f19097ff6cabf5ba910bb6f4e57f23b8d
fengshuonan <sn93@qq.com>
2018-08-15 15:24:54 +0800

    更新tree节点

--- kernel-model/src/main/java/com/stylefeng/roses/kernel/model/node/TreeNode.java
+++ kernel-model/src/main/java/com/stylefeng/roses/kernel/model/node/TreeNode.java
@@ -3,13 +3,13 @@ package com.stylefeng.roses.kernel.model.node;
 import lombok.Data;
 
 /**
- * jquery ztree 插件的节点
+ * tree节点参数的封装
  *
  * @author fengshuonan
  * @date 2017年2月17日 下午8:25:14
  */
 @Data
-public class ZTreeNode {
+public class TreeNode {
 
     private Integer id;     //节点id
 
@@ -22,15 +22,5 @@ public class ZTreeNode {
     private Boolean checked = false;//是否被选中
 
     private Integer isMenu; //是否是菜单(1:是功能权限   2:是操作权限)
-
-    public static ZTreeNode createParent() {
-        ZTreeNode zTreeNode = new ZTreeNode();
-        zTreeNode.setChecked(true);
-        zTreeNode.setId(0);
-        zTreeNode.setName("顶级");
-        zTreeNode.setOpen(true);
-        zTreeNode.setPId(0);
-        return zTreeNode;
-    }
 
 }
