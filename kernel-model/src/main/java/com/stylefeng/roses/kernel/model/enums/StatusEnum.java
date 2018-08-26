9b35dad825184bf86c1238a7e40bd21e4b496ed8
fengshuonan <sn93@qq.com>
2018-08-26 14:08:14 +0800

    更新枚举

--- kernel-model/src/main/java/com/stylefeng/roses/kernel/model/enums/StatusEnum.java
+++ kernel-model/src/main/java/com/stylefeng/roses/kernel/model/enums/StatusEnum.java
@@ -1,22 +1,26 @@
 package com.stylefeng.roses.kernel.model.enums;
 
+import lombok.Getter;
+
 /**
- * 状态的枚举
+ * 启用禁用标识
  *
  * @author fengshuonan
- * @Date 2018年1月23日 15:20:33
+ * @Date 2018/7/24 下午5:31
  */
+@Getter
 public enum StatusEnum {
 
     ENABLE(1, "启用"),
+
     DISABLE(2, "禁用");
 
     private Integer code;
-    private String name;
+    private String desc;
 
-    StatusEnum(Integer code, String name) {
+    StatusEnum(Integer code, String desc) {
         this.code = code;
-        this.name = name;
+        this.desc = desc;
     }
 
     public static String getNameByCode(Integer code) {
@@ -25,18 +29,24 @@ public enum StatusEnum {
         } else {
             for (StatusEnum enumItem : StatusEnum.values()) {
                 if (enumItem.getCode().equals(code)) {
-                    return enumItem.getName();
+                    return enumItem.getDesc();
                 }
             }
             return "";
         }
     }
 
-    public Integer getCode() {
-        return code;
+    public static StatusEnum toEnum(Integer code) {
+        if (null == code) {
+            return null;
+        } else {
+            for (StatusEnum e : StatusEnum.values()) {
+                if (e.getCode().equals(code)) {
+                    return e;
+                }
+            }
+            return null;
+        }
     }
 
-    public String getName() {
-        return name;
-    }
 }
