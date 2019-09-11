d7bcd6ba5a26cc377ca5fcb9d7527567d7a275d5
fengshuonan <sn93@qq.com>
2019-09-11 15:01:27 +0800

    升级2.1.2 更新auth接口

--- kernel-model/src/main/java/cn/stylefeng/roses/kernel/model/api/AuthService.java
+++ kernel-model/src/main/java/cn/stylefeng/roses/kernel/model/api/AuthService.java
@@ -16,48 +16,19 @@
 package cn.stylefeng.roses.kernel.model.api;
 
 import cn.stylefeng.roses.kernel.model.api.model.BaseLoginUser;
-import cn.stylefeng.roses.kernel.model.api.model.LoginReq;
 import cn.stylefeng.roses.kernel.model.api.model.TokenReq;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * 鉴权服务,提供颁发,校验,注销等方法
+ * Auth服务对外接口
  *
  * @author fengshuonan
  * @date 2018-02-07 9:57
  */
 @RequestMapping("/authService")
 public interface AuthService {
-
-    /**
-     * <p>登录(验证账号密码)</p>
-     * <p>若登录成功则返回token,若登录不成功则返回null</p>
-     *
-     * @author fengshuonan
-     * @Date 2018/1/12 13:56
-     */
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    String login(@RequestBody LoginReq loginRequest);
-
-    /**
-     * 校验token(true-校验成功,false-校验失败)
-     *
-     * @author fengshuonan
-     * @Date 2018/2/7 10:02
-     */
-    @RequestMapping(value = "/checkToken", method = RequestMethod.POST)
-    boolean checkToken(@RequestBody TokenReq tokenReq);
-
-    /**
-     * 注销token
-     *
-     * @author fengshuonan
-     * @Date 2018/2/7 10:02
-     */
-    @RequestMapping(value = "/logout", method = RequestMethod.POST)
-    void logout(@RequestBody TokenReq tokenReq);
 
     /**
      * 通过token获取用户信息
